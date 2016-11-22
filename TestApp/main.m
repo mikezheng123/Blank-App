c77c9bda7e911c25c6eff427d49a2cd38d95dd11
Michael Zheng <Michael@michaels-macbook-pro.local>
2016-11-21 18:54:24 -0800

    Initial Commit

---
+++ TestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Michael Zheng on 11/21/16.
+//  Copyright © 2016 BroadcastMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
